51d03f28389c7d9de6f664a1e9be1beef4a7c21e
Igor Belem <igortbelem@gmail.com>
2021-02-25 00:55:20 -0500

    added padding to bottom of round screen opencard

--- screens/RoundAccordion.tsx
+++ screens/RoundAccordion.tsx
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
     borderColor: "#a3a3a3",
     borderRadius: 6,
     borderWidth: 1,
+    paddingBottom: 10,
   },
   closedCard: {
     flexDirection: "column",
